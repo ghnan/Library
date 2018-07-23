1d9ff190f2681fe9c4ebec49294a8b9a9f9bf874
ghnan <122017351@qq.com>
2018-07-23 10:19:07 +0800

    初始化项目

---
+++ MyLibrary/MyLibrary.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyLibrary;
+using MyLibrary.Controllers;
+
+namespace MyLibrary.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+      
+    }
+}
